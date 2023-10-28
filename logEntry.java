3f5053ac52174d010d6eaec3a4c20105f95e3449
zxreyes <148912267+zxreyes@users.noreply.github.com>
2023-10-28 11:06:03 -0700

    Create logEntry.java

---
+++ logEntry.java
@@ -0,0 +1,256 @@
+package prototype;
+
+public class LogEntry {
+	
+	//fields of .csv files
+	private String effortLogProj;
+	private int effortLogProjNum;
+	private int effortNumOfEntries;
+	private int date;
+	private double startTime;
+	private double stopTime;
+	private double deltaTime;
+	private String lifeCycleStep;
+	private int effortCategory;
+	private String effortCategoryName;
+	private String defectLogProj;
+	private int defectLogProjNum;
+	private int defectNumOfEntries;
+	private int defectLogNum;
+	private String defectLogName;
+	private String defectLogDetail;
+	private String stepInjected;
+	private String stepRemoved;
+	private String defectCategory;
+	private String status;
+	private String fix;
+	private String planningPokerProj;
+	private int planningPokerProjSessionNum;
+	private int round;
+	private int sessionDate;
+	private String userStory;
+	private int estimationVals;
+	
+	public LogEntry() {
+		//default constructor
+	}
+
+	//getters and setters for fields
+	
+	public String getField1() {
+		return effortLogProj;
+	}
+
+	public void setField1(String effortLogProj) {
+		this.effortLogProj = effortLogProj;
+	}
+
+	public int getField2() {
+		return effortLogProjNum;
+	}
+
+	public void setField2(int effortLogProjNum) {
+		this.effortLogProjNum = effortLogProjNum;
+	}
+
+	public int getField3() {
+		return effortNumOfEntries;
+	}
+
+	public void setField3(int effortNumOfEntries) {
+		this.effortNumOfEntries = effortNumOfEntries;
+	}
+
+	public int getField4() {
+		return date;
+	}
+
+	public void setField4(int date) {
+		this.date = date;
+	}
+
+	public double getField5() {
+		return startTime;
+	}
+
+	public void setField5(double startTime) {
+		this.startTime = startTime;
+	}
+
+	public double getField6() {
+		return stopTime;
+	}
+
+	public void setField6(double stopTime) {
+		this.stopTime = stopTime;
+	}
+
+	public double getField7() {
+		return deltaTime;
+	}
+
+	public void setField7(double deltaTime) {
+		this.deltaTime = deltaTime;
+	}
+
+	public String getField8() {
+		return lifeCycleStep;
+	}
+
+	public void setField8(String lifeCycleStep) {
+		this.lifeCycleStep = lifeCycleStep;
+	}
+
+	public int getField9() {
+		return effortCategory;
+	}
+
+	public void setField9(int effortCategory) {
+		this.effortCategory = effortCategory;
+	}
+
+	public String getField10() {
+		return effortCategoryName;
+	}
+
+	public void setField10(String effortCategoryName) {
+		this.effortCategoryName = effortCategoryName;
+	}
+
+	public String getField11() {
+		return defectLogProj;
+	}
+
+	public void setField11(String defectLogProj) {
+		this.defectLogProj = defectLogProj;
+	}
+
+	public int getField12() {
+		return defectLogProjNum;
+	}
+
+	public void setField12(int defectLogProjNum) {
+		this.defectLogProjNum = defectLogProjNum;
+	}
+
+	public int getField13() {
+		return defectNumOfEntries;
+	}
+
+	public void setField13(int defectNumOfEntries) {
+		this.defectNumOfEntries = defectNumOfEntries;
+	}
+
+	public int getField14() {
+		return defectLogNum;
+	}
+
+	public void setField14(int defectLogNum) {
+		this.defectLogNum = defectLogNum;
+	}
+
+	public String getField15() {
+		return defectLogName;
+	}
+
+	public void setField15(String defectLogName) {
+		this.defectLogName = defectLogName;
+	}
+
+	public String getField16() {
+		return defectLogDetail;
+	}
+
+	public void setField16(String defectLogDetail) {
+		this.defectLogDetail = defectLogDetail;
+	}
+
+	public String getField17() {
+		return stepInjected;
+	}
+
+	public void setField17(String stepInjected) {
+		this.stepInjected = stepInjected;
+	}
+
+	public String getField18() {
+		return stepRemoved;
+	}
+
+	public void setField18(String stepRemoved) {
+		this.stepRemoved = stepRemoved;
+	}
+
+	public String getField19() {
+		return defectCategory;
+	}
+
+	public void setField19(String defectCategory) {
+		this.defectCategory = defectCategory;
+	}
+
+	public String getField20() {
+		return status;
+	}
+
+	public void setField20(String status) {
+		this.status = status;
+	}
+
+	public String getField21() {
+		return fix;
+	}
+
+	public void setField21(String fix) {
+		this.fix = fix;
+	}
+
+	public String getField22() {
+		return planningPokerProj;
+	}
+
+	public void setField22(String planningPokerProj) {
+		this.planningPokerProj = planningPokerProj;
+	}
+
+	public int getField23() {
+		return planningPokerProjSessionNum;
+	}
+
+	public void setField23(int planningPokerProjSessionNum) {
+		this.planningPokerProjSessionNum = planningPokerProjSessionNum;
+	}
+
+	public int getField24() {
+		return round;
+	}
+
+	public void setField24(int round) {
+		this.round = round;
+	}
+
+	public int getField25() {
+		return sessionDate;
+	}
+
+	public void setField25(int sessionDate) {
+		this.sessionDate = sessionDate;
+	}
+
+	public String getField26() {
+		return userStory;
+	}
+
+	public void setField26(String userStory) {
+		this.userStory = userStory;
+	}
+
+	public int getField27() {
+		return estimationVals;
+	}
+
+	public void setField27(int estimationVals) {
+		this.estimationVals = estimationVals;
+	}
+	
+}
