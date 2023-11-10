5656e14f15ac330eaf8f6a53d0f16d95aab29aaf
Jay Patel <jpate156@asu.edu>
2023-11-10 15:35:34 -0700

    Updated support for initializing, storing, and retrieving definitions data

--- src/Data.java
+++ src/Data.java
@@ -21,7 +21,8 @@ public class Data {
     }
 
     /**
-     * TODO - COMMENT
+     * initializeLogArray()
+     * Description: Allocates memory for log data array. Used in constructor
      */
     private void initializeLogArray() {
         // Init 1st dimension of array (size 10)
@@ -34,21 +35,70 @@ public class Data {
     }
 
     /**
-     * TODO: METHOD COMMENTS
+     * initializeDefArray()
+     * Description: Allocates memory for definitions array. Used in constructor
      */
     private void initializeDefArray() {
-        //TODO: INITIALIZE DEFINITIONS ARRAY
+
+        // Initialize first dimension of definitions array
+        definitions = new ArrayList<>(7); // 7 Definition categories
+
+        // Initialize 2nd dimension of each index
+        for (int i = 0; i < 7; i++) {
+            definitions.add(new ArrayList<>());
+        }
+    }
+
+    /**
+     * getDefinitions(int)
+     * Description: Returns a list of definitions based on the category parameter
+     *         0 = Project name
+     *         1 = Life Cycle Steps
+     *         2 = Effort Categories
+     *         3 = Plans
+     *         4 = Deliverables
+     *         5 = Interruptions
+     *         6 = Defect Categories
+     * @param category - Definition category (0 - 6)
+     * @return returnArr - Arraylist of definitions for specified category
+     */
+    public ArrayList<String> getDefinitions(int category){
+        ArrayList<String> returnArr = new ArrayList<>(); // Stores definitions, will be returned
+
+        // For each definition in category, append info into return array
+        for (int i = 0; i < definitions.get(category).size(); i++) {
+            returnArr.add(definitions.get(category).get(i));
+        }
+        return returnArr;
     }
 
     /**
      * storeLogData(int, String)
-     * Description: TODO: WRITE DESCRIPTION
+     * Description: Stores log data for each specified line in logfile.csv
      * @param projectNum
      * @param currentLine
      */
     public void storeLogData(int projectNum, String currentLine) {
         logData.get(projectNum).add(currentLine);
     }
+
+    /**
+     *
+     * Description:
+     * @param projectNum
+     * @param currentLine
+     */
+    public void storeDefinitionData(int projectNum, String currentLine) {
+        definitions.get(projectNum).add(currentLine);
+    }
+    /**
+     *
+     * @param projectNum
+     * @return
+     */
+    //public ArrayList<String> getLogData(int projectNum){
+
+    //}
 
     /**
      * TODO
@@ -60,6 +110,16 @@ public class Data {
             }
         }
     }
+
+    void printDefData() {
+        for (int i = 0; i < definitions.size(); i++) {
+            for (int j = 0; j < definitions.get(i).size(); j++) {
+                System.out.println(definitions.get(i).get(j) + " ");
+            }
+            System.out.println("---------------------------------------------");
+        }
+    }
+
 
     //public String[] getProjectData(int projectNum) {} //TODO: IMPLEMENT
 }
